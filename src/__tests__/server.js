4a744da2132a806690a3c11594c1958d75d75f59
Gar|k <garik.djan@gmail.com>
2018-09-21 22:40:28 +0500

    rewrite server

---
+++ src/__tests__/server.js
@@ -0,0 +1,96 @@
+require('dotenv').config()
+const { expect } = require('chai')
+const { describe, it } = require('mocha')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const querystring = require('querystring')
+const path = require('path')
+const fs = require('fs')
+
+const SERVER = `http://localhost:${process.env.PORT}`
+const ENDPOINT = {
+  UPLOAD: `${SERVER}/upload/`,
+  REMOVE: `${SERVER}/remove/`
+}
+
+const { apiConstants } = require('../api')
+
+const curl = async (endpoint, params) => {
+
+  let flag = '-F'
+
+  if (params) {
+    params = querystring.stringify(params)
+    params = params.replace('%40', '@')
+    params = params.replace(/%2F/g, '/')
+
+    if (!/@/.test(params)) {
+      flag = '-d'
+    }
+  }
+
+  const { stdout } = await exec(`curl -X POST ${params ? ` ${flag} "${params}"` : null} ${endpoint}`)
+
+  expect(stdout).to.be.a('string')
+
+  try {
+    const json = JSON.parse(stdout)
+    return json
+  } catch (e) {
+    // console.log(stdout)
+    throw e
+  }
+}
+
+function getFilesizeInBytes (filename) {
+  const stats = fs.statSync(filename)
+  const fileSizeInBytes = stats.size
+  return fileSizeInBytes
+}
+
+const filename = path.join(__dirname, '/1.zip')
+const filesize = getFilesizeInBytes(filename)
+
+const checkServer = function (i) {
+  describe(`Check file upload #${i + 1}`, () => {
+    let file
+
+    it(`upload: 1.zip`, async () => {
+      const result = await curl(ENDPOINT.UPLOAD, { file: '@' + filename })
+      // console.log(result)
+
+      /* eslint-disable no-unused-expressions */
+      expect(result[apiConstants.ERROR]).to.be.undefined
+      expect(result[apiConstants.RESPONSE].name).to.equal('1.zip')
+      expect(result[apiConstants.RESPONSE].id).to.be.a('string')
+
+      file = result[apiConstants.RESPONSE]
+    })
+
+    it(`check upload file size: ${filesize}`, () => {
+      const filename = path.join(__dirname, '../../uploads/', file.id)
+      expect(filesize).to.equal(getFilesizeInBytes(filename))
+    })
+
+    it(`no file`, async () => {
+      const result = await curl(ENDPOINT.UPLOAD, { test: 123 })
+      // // console.log(result)
+
+      expect(result[apiConstants.ERROR]).to.equal(apiConstants.ERROR_FILE)
+    })
+
+    it(`remove upload file`, async () => {
+      const result = await curl(ENDPOINT.REMOVE, { id: file.id })
+
+      expect(result[apiConstants.ERROR]).to.be.undefined
+      expect(result[apiConstants.RESPONSE]).to.equal(true)
+
+      const filepath = path.join(__dirname, '../../uploads/', file.id)
+      expect(fs.existsSync(filepath)).to.be.false
+    })
+  })
+}
+
+for (let i = 0; i < 10; i++) {
+  checkServer(i)
+}
---
+++ src/server.js
@@ -0,0 +1,73 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
+const upload = multer({ dest: 'uploads/' })
+const urlencoded = bodyParser.urlencoded({ extended: true })
+const db = require('./db')
+const { apiConstants } = require('./api')
+
+const createDefaultJson = () => {
+  const json = {}
+  json[apiConstants.RESPONSE] = {}
+  return json
+}
+
+app.set('port', process.env.PORT || 3000)
+
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/upload', upload.single('file'), function (req, res) {
+  // req.file is the `file` file
+  // req.body will hold the text fields, if there were any
+
+  const result = createDefaultJson()
+  if (!req.file) {
+    result.error = apiConstants.ERROR_FILE
+  } else {
+    const file = {
+      id: req.file.filename,
+      name: req.file.originalname
+    }
+
+    db.get('files').push(file).write()
+    result.response = file
+  }
+
+  res.type('json')
+  res.json(result)
+})
+
+app.post('/remove', urlencoded, function (req, res) {
+  const result = createDefaultJson()
+
+  if (req.body && req.body.id) {
+    const filepath = path.join(__dirname, '../uploads/', req.body.id)
+    fs.unlinkSync(filepath)
+
+    const isDeleted = !fs.existsSync(filepath)
+
+    if (isDeleted) {
+      db.get('files').remove({ id: req.body.id }).write()
+    }
+
+    result.response = isDeleted
+  } else {
+    result.error = apiConstants.ERROR_PARAMS
+  }
+
+  // console.log(req.body)
+
+  res.type('json')
+  res.json(result)
+})
+
+const server = http.createServer(app)
+server.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'))
+})
